83808a00586b6b0e8832bf9563af68664c1875c1
CHI Jason <jason921008@gmail.com>
2023-03-09 05:01:24 +0900

    second commit

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,21 @@
 import random
-r = random.randint(1, 100)
+start = input("Please decide the random number range start value:")
+end = input("Please decide the random number range end value:")
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 print(r)
+count = 0
 while True:
     num = input("Guess a number:")
     num = int(num)
+    count += 1 #count = count + 1
     if r == num:
         print("You got it!")
+        print("This is your", count, "try.")
         break
     elif r > num:
         print("Smaller than answer")
     else:
         print("Bigger than answer")
+    print("This is your", count, "try.")
